c6d5f169442853aa729a143b4c78c71045c20a44
Serge Stinckwich <Serge.Stinckwich@gmail.com>
2012-04-28 15:10:19 +0700

    tuhuge

--- BrainfuckInterpreter.py
+++ BrainfuckInterpreter.py
@@ -5,6 +5,12 @@ class BrainfuckInterpreterTest(unittest.TestCase):
     def test_the_truth(self):
         self.assertEquals(True, True)
 
-
+    def test_plus_as_a_program_increase_the_first_cell_by_one(self):
+        myInterpreter = BrainfuckInterpreter()
+        before = myInterpreter.cells()[0]
+        myInterpreter.eval('+')
+        after = myInterpreter.cells()[0]
+        expected = before+1
+        self.assertEquals(expected, after)
 if __name__ == "__main__":
 	unittest.main()
